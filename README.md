fcf7662e0a55af7425b5965469265ef204e6223c
Patrik Karlström <patrik@trixon.se>
2020-07-01 15:46:48 +0200

    Rename packages

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 # Trv-Trafikinformation
-Java Objekttyper för Trafikverkets öppna API för trafikinformation
+Java object types for Trafikverkets Traffic Information API
 
 Genererade med xjc
 ```
-xjc -p se.trixon.trv_trafikinformation.jarnvag.railcrossing.v1_4 ../resources/jarnvag_trafikinformation/RailCrossing_v1.4.xsd
-xjc -p se.trixon.trv_trafikinformation.jarnvag.reasoncode.v1 ../resources/jarnvag_trafikinformation/ReasonCode_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.jarnvag.trainannouncement.v1_6 ../resources/jarnvag_trafikinformation/TrainAnnouncement_v1.6.xsd
-xjc -p se.trixon.trv_trafikinformation.jarnvag.trainmessage.v1_6 ../resources/jarnvag_trafikinformation/TrainMessage_v1.6.xsd
-xjc -p se.trixon.trv_trafikinformation.jarnvag.trainstation.v1 ../resources/jarnvag_trafikinformation/TrainStation_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.measurementdata100.v1 ../resources/vag_belaggningsinformation/MeasurementData100_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.measurementdata20.v1 ../resources/vag_belaggningsinformation/MeasurementData20_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.pavementdata.v1 ../resources/vag_belaggningsinformation/PavementData_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.roaddata.v1 ../resources/vag_belaggningsinformation/RoadData_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.roadgeometry.v1 ../resources/vag_belaggningsinformation/RoadGeometry_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.camera.v1 ../resources/vag_trafikinformation/Camera_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.ferryannonuncement.v1_2 ../resources/vag_trafikinformation/FerryAnnouncement_v1.2.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.ferryroute.v1_2 ../resources/vag_trafikinformation/FerryRoute_v1.2.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.icon.v1 ../resources/vag_trafikinformation/Icon_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.parking.v1_4 ../resources/vag_trafikinformation/Parking_v1.4.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.roadconditionoverview.v1 ../resources/vag_trafikinformation/RoadConditionOverview_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.roadcondition.v1_2 ../resources/vag_trafikinformation/RoadCondition_v1.2.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.situation.v1_4 ../resources/vag_trafikinformation/Situation_v1.4.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.trafficflow.v1_4 ../resources/vag_trafikinformation/TrafficFlow_v1.4.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.trafficsafetycamera.v1 ../resources/vag_trafikinformation/TrafficSafetyCamera_v1.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.traveltimeroute.v1_5 ../resources/vag_trafikinformation/TravelTimeRoute_v1.5.xsd
-xjc -p se.trixon.trv_trafikinformation.vag.weatherstation.v1 ../resources/vag_trafikinformation/WeatherStation_v1.xsd
+xjc -p se.trixon.trv_ti.railroad.railcrossing.v1_4 ../resources/jarnvag_trafikinformation/RailCrossing_v1.4.xsd
+xjc -p se.trixon.trv_ti.railroad.reasoncode.v1 ../resources/jarnvag_trafikinformation/ReasonCode_v1.xsd
+xjc -p se.trixon.trv_ti.railroad.trainannouncement.v1_6 ../resources/jarnvag_trafikinformation/TrainAnnouncement_v1.6.xsd
+xjc -p se.trixon.trv_ti.railroad.trainmessage.v1_6 ../resources/jarnvag_trafikinformation/TrainMessage_v1.6.xsd
+xjc -p se.trixon.trv_ti.railroad.trainstation.v1 ../resources/jarnvag_trafikinformation/TrainStation_v1.xsd
+xjc -p se.trixon.trv_ti.road.road.surface.measurementdata100.v1 ../resources/vag_belaggningsinformation/MeasurementData100_v1.xsd
+xjc -p se.trixon.trv_ti.road.road.surface.measurementdata20.v1 ../resources/vag_belaggningsinformation/MeasurementData20_v1.xsd
+xjc -p se.trixon.trv_ti.road.road.surface.pavementdata.v1 ../resources/vag_belaggningsinformation/PavementData_v1.xsd
+xjc -p se.trixon.trv_ti.road.road.surface.roaddata.v1 ../resources/vag_belaggningsinformation/RoadData_v1.xsd
+xjc -p se.trixon.trv_ti.road.road.surface.roadgeometry.v1 ../resources/vag_belaggningsinformation/RoadGeometry_v1.xsd
+xjc -p se.trixon.trv_ti.road.camera.v1 ../resources/vag_trafikinformation/Camera_v1.xsd
+xjc -p se.trixon.trv_ti.road.ferryannonuncement.v1_2 ../resources/vag_trafikinformation/FerryAnnouncement_v1.2.xsd
+xjc -p se.trixon.trv_ti.road.ferryroute.v1_2 ../resources/vag_trafikinformation/FerryRoute_v1.2.xsd
+xjc -p se.trixon.trv_ti.road.icon.v1 ../resources/vag_trafikinformation/Icon_v1.xsd
+xjc -p se.trixon.trv_ti.road.parking.v1_4 ../resources/vag_trafikinformation/Parking_v1.4.xsd
+xjc -p se.trixon.trv_ti.road.roadconditionoverview.v1 ../resources/vag_trafikinformation/RoadConditionOverview_v1.xsd
+xjc -p se.trixon.trv_ti.road.roadcondition.v1_2 ../resources/vag_trafikinformation/RoadCondition_v1.2.xsd
+xjc -p se.trixon.trv_ti.road.situation.v1_4 ../resources/vag_trafikinformation/Situation_v1.4.xsd
+xjc -p se.trixon.trv_ti.road.trafficflow.v1_4 ../resources/vag_trafikinformation/TrafficFlow_v1.4.xsd
+xjc -p se.trixon.trv_ti.road.trafficsafetycamera.v1 ../resources/vag_trafikinformation/TrafficSafetyCamera_v1.xsd
+xjc -p se.trixon.trv_ti.road.traveltimeroute.v1_5 ../resources/vag_trafikinformation/TravelTimeRoute_v1.5.xsd
+xjc -p se.trixon.trv_ti.road.weatherstation.v1 ../resources/vag_trafikinformation/WeatherStation_v1.xsd
 ```
---
+++ src/main/java/se/trixon/trv_ti/TrafficInformation.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright 2020 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.trv_ti;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class TrafficInformation {
+
+    private final HashMap<Class, Unmarshaller> mClassToUnmarshaller = new HashMap<>();
+    private final HttpClient mHttpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+    private String mKey = "";
+    private final Railroad mRailroad = new Railroad();
+    private final Road mRoad = new Road();
+    private int mTimeout = 5000;
+    private String mUrl = "https://api.trafikinfo.trafikverket.se/v2/data.xml";
+    private final String requestTemplate = "<REQUEST>\n"
+            + "  <LOGIN authenticationkey=\"%s\" />\n"
+            + "  <QUERY%s>\n"
+            + "    %s\n"
+            + "  </QUERY>\n"
+            + "</REQUEST>";
+
+    public TrafficInformation() {
+    }
+
+    public TrafficInformation(String key) {
+        mKey = key;
+    }
+
+    public TreeMap<String, String> createQueryAttributes() {
+        return new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    }
+
+    public HttpResponse<String> getHttpResponse(String requestString) throws IOException, InterruptedException {
+        Logger.getLogger(TrafficInformation.class.getName()).log(Level.INFO, requestString);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(requestString))
+                .uri(URI.create(mUrl))
+                .header("Content-Type", "text/xml")
+                .timeout(Duration.ofMillis(mTimeout))
+                .build();
+
+        HttpResponse<String> response = mHttpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response;
+    }
+
+    public String getKey() {
+        return mKey;
+    }
+
+    public <T> T getResponse(Class<T> clazz, File file) throws IOException, InterruptedException, JAXBException {
+        return ((JAXBElement<T>) getUnmarshaller(clazz).unmarshal(file)).getValue();
+    }
+
+    public <T> T getResponse(Class<T> clazz, String queryDetails, File file) throws IOException, InterruptedException, JAXBException {
+        HttpResponse<String> r = getHttpResponse(queryDetails);
+        String s = r.body();
+
+        if (file != null) {
+            Files.writeString(file.toPath(), s, Charset.forName("utf-8"));
+//            FileUtils.writeStringToFile(file, s, "utf-8");
+
+        }
+
+        return ((JAXBElement<T>) getUnmarshaller(clazz).unmarshal(new StringReader(s))).getValue();
+    }
+
+    public int getTimeout() {
+        return mTimeout;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public Railroad railroad() {
+        return mRailroad;
+    }
+
+    public Road road() {
+        return mRoad;
+    }
+
+    public void setKey(String key) {
+        mKey = key;
+    }
+
+    public void setTimeout(int timeout) {
+        mTimeout = timeout;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    private String getRequest(TreeMap<String, String> queryAttributes, String objecttype, String schemaversion, String queryDetails) {
+        if (queryAttributes == null) {
+            queryAttributes = createQueryAttributes();
+        }
+        queryAttributes.putIfAbsent("objecttype", objecttype);
+        queryAttributes.putIfAbsent("schemaversion", schemaversion);
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : queryAttributes.entrySet()) {
+            sb.append(String.format(" %s=\"%s\"", entry.getKey(), entry.getValue()));
+        }
+
+        if (queryDetails == null) {
+            queryDetails = "";
+        }
+
+        return String.format(requestTemplate, mKey, sb.toString(), queryDetails);
+    }
+
+    private Unmarshaller getUnmarshaller(Class clazz) {
+        Unmarshaller unmarshaller = mClassToUnmarshaller.computeIfAbsent(clazz, k -> {
+            try {
+                return JAXBContext.newInstance(k).createUnmarshaller();
+            } catch (JAXBException ex) {
+                Logger.getLogger(TrafficInformation.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
+            }
+        });
+
+        return unmarshaller;
+    }
+
+    public class Railroad {
+
+        public List<se.trixon.trv_ti.railroad.trainmessage.v1_6.RESULT> getTrainMessageResults(TreeMap<String, String> queryAttributes, String queryDetails, File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.railroad.trainmessage.v1_6.RESPONSE.class,
+                    getRequest(queryAttributes, "TrainMessage", "1.6", queryDetails), file).getRESULT();
+        }
+
+        public List<se.trixon.trv_ti.railroad.trainmessage.v1_6.RESULT> getTrainMessageResults(File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.railroad.trainmessage.v1_6.RESPONSE.class,
+                    file).getRESULT();
+        }
+
+        public List<se.trixon.trv_ti.railroad.trainstation.v1.RESULT> getTrainStationResults(TreeMap<String, String> queryAttributes, String queryDetails, File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.railroad.trainstation.v1.RESPONSE.class,
+                    getRequest(queryAttributes, "TrainStation", "1", queryDetails), file).getRESULT();
+        }
+
+        public List<se.trixon.trv_ti.railroad.trainstation.v1.RESULT> getTrainStationResults(File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.railroad.trainstation.v1.RESPONSE.class,
+                    file).getRESULT();
+        }
+    }
+
+    public class Road {
+
+        private final Surcface mSurcface = new Surcface();
+
+        public List<se.trixon.trv_ti.road.camera.v1.RESULT> getCameraResults(TreeMap<String, String> queryAttributes, String queryDetails, File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.road.camera.v1.RESPONSE.class,
+                    getRequest(queryAttributes, "Camera", "1", queryDetails), file).getRESULT();
+        }
+
+        public List<se.trixon.trv_ti.road.camera.v1.RESULT> getCameraResults(File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.road.camera.v1.RESPONSE.class,
+                    file).getRESULT();
+        }
+
+        public List<se.trixon.trv_ti.road.trafficsafetycamera.v1.RESULT> getTrafficSafetyCameraResults(TreeMap<String, String> queryAttributes, String queryDetails, File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.road.trafficsafetycamera.v1.RESPONSE.class,
+                    getRequest(queryAttributes, "TrafficSafetyCamera", "1", queryDetails), file).getRESULT();
+        }
+
+        public List<se.trixon.trv_ti.road.trafficsafetycamera.v1.RESULT> getTrafficSafetyCameraResults(File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.road.trafficsafetycamera.v1.RESPONSE.class,
+                    file).getRESULT();
+        }
+
+        public List<se.trixon.trv_ti.road.weatherstation.v1.RESULT> getWeatherStationResults(TreeMap<String, String> queryAttributes, String queryDetails, File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.road.weatherstation.v1.RESPONSE.class,
+                    getRequest(queryAttributes, "WeatherStation", "1", queryDetails), file).getRESULT();
+        }
+
+        public List<se.trixon.trv_ti.road.weatherstation.v1.RESULT> getWeatherStationResults(File file) throws IOException, InterruptedException, JAXBException {
+            return getResponse(se.trixon.trv_ti.road.weatherstation.v1.RESPONSE.class,
+                    file).getRESULT();
+        }
+
+        public Surcface surface() {
+            return mSurcface;
+        }
+
+        public class Surcface {
+
+        }
+    }
+
+}
